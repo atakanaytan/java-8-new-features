3d14b811dd3f94706e056d9606476d78cd7e6073
atakanaytan <aytan.atakan2@gmail.com>
2021-05-15 15:57:38 +0300

    stream API - flatMap() operation

---
+++ src/streams/StreamsFlatMapExample.java
@@ -0,0 +1,25 @@
+package streams;
+
+import data.Student;
+import data.StudentDatabase;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+public class StreamsFlatMapExample {
+
+    public static List<String> printStudentActivities() {
+
+        List<String> studentActivities = StudentDatabase.getAllStudents().stream()
+                .map(Student::getActivities)
+                .flatMap(List::stream)
+                .collect(toList());
+
+        return studentActivities;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(printStudentActivities());
+    }
+}
